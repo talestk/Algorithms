2a3c586de1ac15b60134bd51a3ac73bc15d4aac5
Tales Takemiya <talestk@hotmail.com>
2012-08-20 17:03:13 -0500

    Adding Palindrome Checker

---
+++ src/Palindrome.java
@@ -0,0 +1,23 @@
+
+public class Palindrome {
+	
+	public static boolean isPalindrome(String s) {
+		int N = s.length();
+		for (int i = 0; i < N/2; i++) {
+			if(s.charAt(i) != s.charAt(N-1-i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Palindrome");
+		
+		if(isPalindrome("socorrammesubinoonibusemmarrocos") == true) {
+			System.out.println("True!");
+		} else { System.out.println("Not True!"); }
+	}
+
+}
--- src/Recursion.java
+++ src/Recursion.java
@@ -13,23 +13,4 @@ public class Recursion {
 		System.out.println(n);
 		return exR1(n-3) + n + exR1(n-2) + n;
 	}
-	
-	public static int rank(int key, int[] a) {
-		return rank(key, a, 0, a.length -1);
-	}
-	
-	public static int rank(int key, int[] a, int lo, int hi) {
-		if (lo > hi) {
-			return -1;
-		}
-		int mid = lo +(hi - lo) / 2;
-		if (key < a[mid]) { 
-			return rank(key,a,lo, mid-1);
-		} 
-		else if(key > a[mid]) { 
-			return rank(key,a,mid+1, hi);
-		} else return mid;
-	
-	}
-
 }
